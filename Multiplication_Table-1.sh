eb0e07e6f14949121cdac4c5612dc619bfe3259a
EunjiKang <kkangji87@knou.ac.kr>
2022-05-25 22:12:06 +0900

    1st commit: Multiplication_Table-1.sh created

---
+++ Multiplication_Table-1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo Multiplication Table
+for a in 1 2 3 4 5 6 7 8 9
+do
+        for b in 1 2 3 4 5 6 7 8 9
+        do
+                R=`expr $a \* $b`
+                echo $a "x" $b "=" $R
+                continue
+        done
+done
+~   
+
